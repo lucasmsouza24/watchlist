712aecc9f20f2c9b45d76d36e0a0a20670ad3f1d
Lucas Mesquita <lucas.msouza@bandtec.com.br>
2021-05-11 00:21:49 -0300

    adicionada página do título(incompleta)

--- js/build-index-content.js
+++ js/build-index-content.js
@@ -3,6 +3,8 @@ for (let i = 0; i < 5; i++) {
     for (let j = 0; j < 5; j++) {
         let card = createCard("Fumetsu no Anata E", "./img/data/poster/exemple-poster.jpg");
         let content = box.childNodes[1];
+        let redirect = `redirectTitle(${j})`;
+        card.setAttribute("onclick", redirect);
         
         // add element to content
         content.appendChild(card);
