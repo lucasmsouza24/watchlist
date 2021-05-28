608fc00a7b87a651bb11c157be584c6698052869
Lucas Mesquita <lucas.msouza@bandtec.com.br>
2021-05-28 01:14:11 -0300

    alterações visuais no menu e novas cores de cards e boxs

--- API-projeto-site/public/js/build-menu.js
+++ API-projeto-site/public/js/build-menu.js
@@ -10,7 +10,6 @@ if (localStorage.getItem("watchlist_user") !== null) {
         <li class="main-menu-item main-menu-item-click"><a href="category.html?type=tvserie">Séries</a></li>
         <li class="main-menu-item main-menu-item-click"><a href="category.html?type=anime">Animes</a></li>
         <li><input class="menu-search-input" placeholder="Pesquisar" id="inputSearch"> <button class="btn btn-green bg-dblue" onclick="search()">Pesquisar</button></li>
-        <li class="main-menu-item main-menu-item-click margin-left"><a href="#">Sobre</a></li>
         <li class="main-menu-item main-menu-item-click main-menu-item-entre"><a href="./user-page.html">@${user.nick}</a></li>
     </ul>
     `;
@@ -20,10 +19,10 @@ if (localStorage.getItem("watchlist_user") !== null) {
     idMenuBox.innerHTML = `
     <ul class="main-menu-ul">
         <li class="main-menu-item"><a href="index.html"><img src="./img/logotipo-watchlist.png" alt="watchlist logo"></a></li>
-        <li class="main-menu-item main-menu-item-click"><a href="#">Filmes</a></li>
-        <li class="main-menu-item main-menu-item-click"><a href="#">Séries</a></li>
-        <li class="main-menu-item main-menu-item-click"><a href="#">Animes</a></li>
-        <li class="main-menu-item main-menu-item-click margin-left"><a href="#">Sobre</a></li>
+        <li class="main-menu-item main-menu-item-click"><a href="category.html?type=movie">Filmes</a></li>
+        <li class="main-menu-item main-menu-item-click"><a href="category.html?type=tvserie">Séries</a></li>
+        <li class="main-menu-item main-menu-item-click"><a href="category.html?type=anime">Animes</a></li>
+        <li><input class="menu-search-input" placeholder="Pesquisar" id="inputSearch"> <button class="btn btn-green bg-dblue" onclick="search()">Pesquisar</button></li>
         <li class="main-menu-item main-menu-item-click main-menu-item-entre"><a href="./login.html">Entrar</a></li>
         <li class="main-menu-item main-menu-item-click"><a href="sign-up.html">Cadastrar</a></li>
     </ul>
